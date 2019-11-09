512c47e73c122b7c10b2effa562750ca964fd86a
Hans Koch <mail@hans-koch.me>
2019-11-09 19:54:21 +0100

    pants for the baby

--- Baby.gd
+++ Baby.gd
@@ -2,15 +2,19 @@ extends Node2D
 
 var steps_done = 0
 var done = false
+var babySprite = null
+export(NodePath) var babySpritePath
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	babySprite = get_node(babySpritePath)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if steps_done == 3:
 		done = true
+		babySprite.frame = 1
 		print("done")
 	pass
 
@@ -23,6 +27,7 @@ func reset():
 	$Dot/Sprite.frame = 0
 	$Dot2/Sprite.frame = 0
 	$Dot3/Sprite.frame = 0
+	babySprite.frame = 0
 	done = false
 
 func show():
